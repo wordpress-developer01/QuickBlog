0d1e83d02d2ad09f14c83a4f7404a09c5431f449
wordpress-developer01 <130246057+wordpress-developer01@users.noreply.github.com>
2026-04-05 14:10:10 +0300

    first commit

---
+++ server/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import connectDB from './configs/db.js';
+import adminRouter from './routes/adminRoutes.js';
+import blogRouter from './routes/blogRoutes.js';
+
+
+const app = express();
+
+await connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res)=> res.send("API is Working"))
+app.use('/api/admin', adminRouter)
+app.use('/api/blog', blogRouter)
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, ()=>{
+    console.log('Server is running on port ' + PORT)
+})
+
+export default app;
